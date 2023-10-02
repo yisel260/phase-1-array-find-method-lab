ffae7fc2087ed4c52a23626bf9a176b5d6e17824
Yisel Cintora Vega <yiselcintoravega@gmail.com>
2023-10-02 19:51:45 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function superbowlWin(element,index,array) {
+    for (const record of element ){
+        if (record.result === "W"){
+            return record.year
+        }
+        
+    }
+}
+
+record.find(superbowlWin)
+
+
+
+
+
